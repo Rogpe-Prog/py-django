4e05c130b443d73d21f81007912506380cd13993
Rogpe-Prog <paulo.rogpe@gmail.com>
2025-11-09 14:09:56 -0300

    base dir, urls and render

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Receitas',
+    })
 
 def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/sobre.html')
